f85508cf64c341b58337377fe7ebf71b471f6c1e
carojas98 <carlos.frojas.98@gmail.com>
2025-04-05 19:11:08 -0500

    Apertura del navegador

--- kid_bot.py
+++ kid_bot.py
@@ -14,3 +14,16 @@ import time
 import urllib.parse 
 
 #Configuración Navegador
+
+def portal_dimensional():
+    opciones = Options()
+    opciones.add_argument("--window.size=1100,700")
+    opciones.add_argument("--disable-notifications")
+
+    servicio = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=servicio,options=opciones)
+
+navegador = portal_dimensional()
+navegador.get("https://www.google.com")
+input("Presiona Enter para cerrar el navegador...")
+navegador.quit 
